1392fac402aa2517a98b2fdc2ec9c92a6277c579
Julia Skoneczna <juliaskoneczna@gmail.com>
2021-04-14 21:16:44 +0200

    Improved test structure

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tests
 
 if __name__ == '__main__':
 
-    tests.test_iterations(10, 2, 3, 10, 5, 0.5)
+    tests.test_iterations(start_iterations=100, iterations_step=200, number_of_tests=3,
+                          population_size=50, mutation_strength=5, crossover_probability=0.5)
 
     # print('Your score: {}'.format(game.play(brain=best_network, graphical=True)))
--- tests.py
+++ tests.py
@@ -19,19 +19,19 @@ def test(figure_number, population_size, mutation_strength, crossover_probabilit
     # print('Your score: {}'.format(game.play(brain=best_network, graphical=True)))
     best_scores = [el[0] for el in results]
     average_scores = [el[1] for el in results]
-    plt.scatter(iterations, best_scores, s=20)
+    best = plt.scatter(iterations, best_scores, s=5, label='najlepszy wynik')
     plt.xlabel('Iteracja')
     plt.ylabel('Osiągnięty wynik')
 
-    plt.scatter(iterations, average_scores, s=20)
-    plt.legend(['najlepszy wynik', 'średni wynik'], loc='lower right')
+    average = plt.scatter(iterations, average_scores, s=5, label='średni wynik')
+    plt.legend(handles=[best, average], bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=2)
     current_directory = os.path.dirname(__file__)
     filename = chart_name + str(figure_number) + '.png'
     plt.savefig(current_directory + '/charts/' + filename)
 
 
-def test_iterations(start_iterations, iterations_step, number_of_tests,
-                    population_size, mutation_strength, crossover_probability):
+def test_iterations(start_iterations=10, iterations_step=2, number_of_tests=3,
+                    population_size=10, mutation_strength=10, crossover_probability=0.4):
     current_figure = 1
     current_iterations = start_iterations
     # tests depending on the number of iterations
